41df2ad1b8961774b2a09a6be387ba4302053fba
Jerome Perona <jeromeperona@yahoo.com>
2022-08-08 14:32:37 +0800

    login page with udpate in base

--- todolist/app.py
+++ todolist/app.py
@@ -1,16 +1,15 @@
 from todowebapp import create_app
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template
 import sqlite3
-
 
 app = Flask(__name__)
 app = create_app()
 
-# con = sqlite3.connect("todo.db")  
+con = sqlite3.connect("todo.db")  
 # print("Database opened successfully")  
 # con.execute("create table User (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT  UNIQUE NOT NULL, password TEXT NOT NULL, confirmPassword TEXT NOT NULL)")  
 # print("Table created successfully")  
-
+# con.execute("create table ToDo (user_id INTEGER , todo_id INTEGER PRIMARY KEY AUTOINCREMENT, todo_name TEXT NOT NULL, date DATE NOT NULL)")
 # con = sqlite3.connect('todo.db')  
 # con.row_factory = sqlite3.Row  
 # cur = con.cursor()
@@ -19,32 +18,7 @@ app = create_app()
 def index():
 
     return render_template("index.html")
-    
-@app.route("/signup", methods=['POST','GET'])
-def signUp():
 
-    with sqlite3.connect("todo.db") as con:
-        cur = con.cursor()
-
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        confirmPassword = request.form.get('confirmPassword')
-
-        statement = f"SELECT * FROM User WHERE username='{username}';"
-        cur.execute(statement)
-        data = cur.fetchone()
-
-        if data:
-            flash("This email was already taken.", category='e')
-        
-        else:
-            cur.execute("INSERT into User (username, password, confirmPassword) values (?,?,?)",(username, password, confirmPassword))
-            con.commit()
-
-            flash("Successfully registered.", category='s')
-
-    return render_template("signup.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
--- todolist/todowebapp/auth.py
+++ todolist/todowebapp/auth.py
@@ -1,3 +1,63 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, flash, render_template, request, redirect, url_for, session
+import sqlite3
 
 auth = Blueprint('auth', __name__)
+
+
+@auth.route("/signup", methods=['POST','GET'])
+def signUp():
+
+    with sqlite3.connect("todo.db") as con:
+        cur = con.cursor()
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmPassword = request.form.get('confirmPassword')
+
+        statement = f"SELECT * FROM User WHERE username='{username}';"
+        cur.execute(statement)
+        data = cur.fetchone()
+
+        if data:
+            flash("This email was already taken.", category='error')
+        
+        else:
+            cur.execute("INSERT into User (username, password, confirmPassword) values (?,?,?)",(username, password, confirmPassword))
+            con.commit()
+
+            flash("Successfully registered.", category='success')
+            return redirect(url_for("auth.login"))
+
+    return render_template("signup.html")
+
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+
+    with sqlite3.connect("todo.db") as con:
+        cur = con.cursor()
+
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        statement = f"SELECT * FROM User WHERE username='{username}' AND password='{password}';"
+        cur.execute(statement)
+
+        if not cur.fetchone():
+            flash("Your username or password was incorrect.", category='error')    
+            return redirect(url_for("auth.login"))
+        
+        else: 
+            session["user"] = username
+            return redirect(url_for("auth.mytodolist"))
+    
+    return render_template("login.html")
+
+
+@auth.route("mytodolist")
+def mytodolist():
+
+    return render_template("mytodolist.html")
+
+
